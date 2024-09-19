19bc4c24e42102aeb0bcfc7e5d3153b4a2c2ce21
ehsan <ganimehsan@gmail.com>
2024-09-19 11:09:50 -0700

    updated data_laoder need fixing with singleton

--- data_loader.py
+++ data_loader.py
@@ -1,12 +1,13 @@
 import os
 from DocumentsParser import extract_text_from_file
-
+from AudioTranscription import transcribe_and_save
 
 DOCS_DIRECTORY = './docs'
 
 # List all files in the directory
-uploaded = [os.path.join(DOCS_DIRECTORY, filename) for filename in os.listdir(DOCS_DIRECTORY) if filename.endswith(('.docx', '.pdf', '.txt', '.pptx'))]
-
+uploaded = [os.path.join(DOCS_DIRECTORY, filename) for filename in os.listdir(DOCS_DIRECTORY) if filename.endswith(('.docx', '.pdf', '.pptx'))]
+audio_files = [os.path.join(DOCS_DIRECTORY, filename) for filename in os.listdir(DOCS_DIRECTORY) 
+               if filename.endswith(('.mp3'))]
 # Process all uploaded files
 for idx, filename in enumerate(uploaded):
     print(f"Processing File {idx + 1}/{len(uploaded)}: {filename}")
@@ -17,5 +18,13 @@ for idx, filename in enumerate(uploaded):
         else:
             print(f"Successfully processed {filename}: {len(text)} characters extracted")
 
+    except Exception as e:
+        print(f"Error processing {filename}: {e}")
+
+for idx, filename in enumerate(audio_files):
+    print(f"Processing Audio File {idx + 1}/{len(audio_files)}: {filename}")
+    try:
+        transcribe_and_save(filename)
+        print(f"Successfully transcribed {filename}")
     except Exception as e:
         print(f"Error processing {filename}: {e}")
